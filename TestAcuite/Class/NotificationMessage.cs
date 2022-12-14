52a01d2a2da12a3fb92eb05dcb922daa969b41b9
Antjac <antjac.tux@free.fr>
2022-12-14 12:12:03 +0100

    Add files via upload

---
+++ TestAcuite/Class/NotificationMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAcuite.Class
+{
+    public class NotificationMessage : ValueChangedMessage<String>
+    {
+        public NotificationMessage(String message) : base(message)
+        {
+        }
+    }
+    
+}
